a3eeba97097a3765b6af674392c7e2afca5c222b
Nicole Hu <nicoleyghu@gmail.comu>
2021-03-03 20:31:58 -0500

    Subsampling PCA implementation.

--- amptorch/trainer.py
+++ amptorch/trainer.py
@@ -11,10 +11,8 @@ import torch
 from skorch import NeuralNetRegressor
 from skorch.callbacks import LRScheduler
 from skorch.dataset import CVSplit
-from collections import OrderedDict
 
 from amptorch.dataset import AtomsDataset, DataCollater, construct_descriptor
-from amptorch.dataset_lmdb import AtomsLMDBDataset
 from amptorch.descriptor.util import list_symbols_to_indices
 from amptorch.metrics import evaluator
 from amptorch.model import BPNN, SingleNN, CustomLoss
@@ -83,26 +81,18 @@ class AtomsTrainer:
         return elements
 
     def load_dataset(self):
-        if "lmdb_path" in self.config["dataset"]:
-            self.train_dataset = AtomsLMDBDataset(
-                self.config["dataset"]["lmdb_path"],
-            )
-            self.elements = self.train_dataset.elements
-            descriptor_setup = self.train_dataset.descriptor_setup
-        else:
-            training_images = self.config["dataset"]["raw_data"]
-            # TODO: Scalability when dataset to large to fit into memory
-            if isinstance(training_images, str):
-                training_images = ase.io.read(training_images, ":")
-            del self.config["dataset"]["raw_data"]
+        training_images = self.config["dataset"]["raw_data"]
+        # TODO: Scalability when dataset to large to fit into memory
+        if isinstance(training_images, str):
+            training_images = ase.io.read(training_images, ":")
+        del self.config["dataset"]["raw_data"]
 
-            self.elements = self.config["dataset"].get(
-                "elements", self.get_unique_elements(training_images)
-            )
+        self.elements = self.config["dataset"].get(
+            "elements", self.get_unique_elements(training_images)
+        )
 
-<<<<<<< HEAD
         self.forcetraining = self.config["model"].get("get_forces", True)
-        self.pca_reduce = self.config["model"].get("pca_reduce", False)
+        self.pca_reduce = self.config["dataset"].get("pca_reduce", False)
         self.fp_scheme = self.config["dataset"].get("fp_scheme", "gaussian").lower()
         self.fp_params = self.config["dataset"]["fp_params"]
         self.save_fps = self.config["dataset"].get("save_fps", True)
@@ -133,41 +123,10 @@ class AtomsTrainer:
                 {"type": "normalize", "range": (0, 1), "elementwise": True},
             ),
         )
-=======
-            self.forcetraining = self.config["model"].get("get_forces", True)
-            self.pca_reduce = self.config["dataset"].get("pca_reduce", False)
-            self.fp_scheme = self.config["dataset"].get("fp_scheme", "gaussian").lower()
-            self.fp_params = self.config["dataset"]["fp_params"]
-            self.save_fps = self.config["dataset"].get("save_fps", True)
-            self.cutoff_params = self.config["dataset"].get(
-                "cutoff_params", {"cutoff_func": "Cosine"}
-            )
-            descriptor_setup = (
-                self.fp_scheme,
-                self.fp_params,
-                self.cutoff_params,
-                self.elements,
-            )
-            self.train_dataset = AtomsDataset(
-                images=training_images,
-                descriptor_setup=descriptor_setup,
-                forcetraining=self.forcetraining,
-                pca_reduce = self.pca_reduce,
-                pca_setting = self.config["dataset"].get(
-                    "pca_setting",
-                    {"num_pc": 20, "elementwise": False, "normalize": False}
-                ),
-                save_fps=self.config["dataset"].get("save_fps", True),
-                scaling=self.config["dataset"].get(
-                    "scaling",
-                    {"type": "normalize", "range": (0, 1), "elementwise": True},
-                ),
-            )
->>>>>>> 0b38ea9decb36f6ec6f227eb0c1ae528bb6c4cd9
         self.feature_scaler = self.train_dataset.feature_scaler
         self.target_scaler = self.train_dataset.target_scaler
-        # if self.pca_reduce:
-        #     self.pca_reducer = self.train_dataset.pca_reducer
+        if self.pca_reduce:
+            self.pca_reducer = self.train_dataset.pca_reducer
         self.input_dim = self.train_dataset.input_dim
         self.val_split = self.config["dataset"].get("val_split", 0)
         if not self.debug:
@@ -176,10 +135,10 @@ class AtomsTrainer:
                 "feature": self.feature_scaler,
             }
             torch.save(normalizers, os.path.join(self.cp_dir, "normalizers.pt"))
-            # if self.pca_reduce:
-            #     torch.save(self.pca_reducer, os.path.join(self.cp_dir, "pca_reducer.pt"))
+            if self.pca_reduce:
+                torch.save(self.pca_reducer, os.path.join(self.cp_dir, "pca_reducer.pt"))
             # clean/organize config
-            # del self.config["dataset"]["fp_params"]
+            del self.config["dataset"]["fp_params"]
             self.config["dataset"]["descriptor"] = descriptor_setup
             self.config["dataset"]["fp_length"] = self.input_dim
             torch.save(self.config, os.path.join(self.cp_dir, "config.pt"))
@@ -245,12 +204,7 @@ class AtomsTrainer:
         self.criterion = self.config["optim"].get("loss_fn", CustomLoss)
 
     def load_optimizer(self):
-        self.optimizer = {
-            "optimizer": self.config["optim"].get("optimizer", torch.optim.Adam)
-        }
-        optimizer_args = self.config["optim"].get("optimizer_args", False)
-        if optimizer_args:
-            self.optimizer.update(optimizer_args)
+        self.optimizer = self.config["optim"].get("optimizer", torch.optim.Adam)
 
     def load_logger(self):
         if self.config["cmd"].get("logger", False):
@@ -259,6 +213,7 @@ class AtomsTrainer:
             self.wandb_run = wandb.init(
                 name=self.identifier,
                 config=self.config,
+                id=self.timestamp,
             )
 
     def load_skorch(self):
@@ -271,11 +226,12 @@ class AtomsTrainer:
                 "force_coefficient", 0
             ),
             criterion__loss=self.config["optim"].get("loss", "mse"),
+            optimizer=self.optimizer,
             lr=self.config["optim"].get("lr", 1e-1),
             batch_size=self.config["optim"].get("batch_size", 32),
             max_epochs=self.config["optim"].get("epochs", 100),
             iterator_train__collate_fn=self.parallel_collater,
-            iterator_train__shuffle=False,
+            iterator_train__shuffle=True,
             iterator_train__pin_memory=True,
             iterator_valid__collate_fn=self.parallel_collater,
             iterator_valid__shuffle=False,
@@ -284,7 +240,6 @@ class AtomsTrainer:
             train_split=self.split,
             callbacks=self.callbacks,
             verbose=self.config["cmd"].get("verbose", True),
-            **self.optimizer,
         )
         print("Loading skorch trainer")
 
@@ -299,7 +254,7 @@ class AtomsTrainer:
         elapsed_time = time.time() - stime
         print(f"Training completed in {elapsed_time}s")
 
-    def predict(self, images, disable_tqdm=True):
+    def predict(self, images, batch_size=32):
         if len(images) < 1:
             warnings.warn("No images found!", stacklevel=2)
             return images
@@ -341,14 +296,7 @@ class AtomsTrainer:
 
         return predictions
 
-    def load_pretrained(self, checkpoint_path=None, gpu2cpu=False):
-        """
-        Args:
-            checkpoint_path: str, Path to checkpoint directory
-            gpu2cpu: bool, True if checkpoint was trained with GPUs and you
-            wish to load on cpu instead.
-        """
-
+    def load_pretrained(self, checkpoint_path=None):
         self.pretrained = True
         print(f"Loading checkpoint from {checkpoint_path}")
         assert os.path.isdir(
@@ -360,32 +308,14 @@ class AtomsTrainer:
             self.config["cmd"]["debug"] = True
             self.elements = self.config["dataset"]["descriptor"][-1]
             self.input_dim = self.config["dataset"]["fp_length"]
-            if gpu2cpu:
-                self.config["optim"]["gpus"] = 0
             self.load(load_dataset=False)
         else:
             # prediction+retraining
             self.load(load_dataset=True)
         self.net.initialize()
-
-        if gpu2cpu:
-            params_path = os.path.join(checkpoint_path, "params_cpu.pt")
-            if not os.path.exists(params_path):
-                params = torch.load(
-                    os.path.join(checkpoint_path, "params.pt"),
-                    map_location=torch.device("cpu"),
-                )
-                new_dict = OrderedDict()
-                for k, v in params.items():
-                    name = k[7:]
-                    new_dict[name] = v
-                torch.save(new_dict, params_path)
-        else:
-            params_path = os.path.join(checkpoint_path, "params.pt")
-
         try:
             self.net.load_params(
-                f_params=params_path,
+                f_params=os.path.join(checkpoint_path, "params.pt"),
                 f_optimizer=os.path.join(checkpoint_path, "optimizer.pt"),
                 f_criterion=os.path.join(checkpoint_path, "criterion.pt"),
                 f_history=os.path.join(checkpoint_path, "history.json"),
